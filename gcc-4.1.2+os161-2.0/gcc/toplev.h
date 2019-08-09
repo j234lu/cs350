583061c401502aed22a9b7d4454b35a5ca050136
oscarlu10000@hotmail.com <oscarlu10000@hotmail.com>
2019-08-08 23:40:50 -0400

    gcc install successfully

--- gcc-4.1.2+os161-2.0/gcc/toplev.h
+++ gcc-4.1.2+os161-2.0/gcc/toplev.h
@@ -1,19 +1,15 @@
 /* toplev.h - Various declarations for functions found in toplev.c
    Copyright (C) 1998, 1999, 2000, 2001, 2003, 2004, 2005
    Free Software Foundation, Inc.
-
 This file is part of GCC.
-
 GCC is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free
 Software Foundation; either version 2, or (at your option) any later
 version.
-
 GCC is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 for more details.
-
 You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING.  If not, write to the Free
 Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
